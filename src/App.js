e61394090d0e371183b0b137e7cbb8d8fb35e421
Eric Camalionte <eric80hz@yahoo.com.br>
2018-05-23 11:45:05 -0300

    List and save authors

--- src/App.js
+++ src/App.js
@@ -5,9 +5,26 @@ import './css/side-menu.css';
 import $ from 'jquery';
 
 class App extends Component {
+
     constructor() {
         super();
-        this.state = { lista :[] };
+        this.state = { lista: [], nome: '', email: '', senha: '' };
+        this.submitForm = this.submitForm.bind(this);
+        this.setNome = this.setNome.bind(this);
+        this.setEmail = this.setEmail.bind(this);
+        this.setSenha = this.setSenha.bind(this);
+    }
+
+    setNome(event) {
+        this.setState({nome: event.target.value});
+    }
+
+    setEmail(event){
+        this.setState({email: event.target.value});
+    }
+
+    setSenha(event){
+        this.setState({senha: event.target.value});
     }
 
     componentWillMount(){
@@ -17,14 +34,39 @@ class App extends Component {
             success: function(data) {
                 console.log("response received!");
                 this.setState({lista: data});
-            }.bind(this)
+            }.bind(this),
+            error: function(data) {
+                console.log("error" + data);
+            }
           }
         );
     }
 
+    submitForm(event){
+        console.log('sending data...');
+        event.preventDefault();
+        $.ajax({
+            url: 'http://cdc-react.herokuapp.com/api/autores',
+            contentType: 'application/json',
+            dataType: 'json',
+            type: 'post',
+            data: JSON.stringify({
+                nome: this.state.nome,
+                email: this.state.email,
+                senha: this.state.senha}),
+            success: function(data) {
+                console.log("success" + data);
+                this.setState({ lista: data});
+            }.bind(this),
+            error: function(data) {
+                console.log("error" + data.toString());
+            }
+        });
+    }
+
 
     render() {
-        console.log("rendering...")
+        console.log("rendering...");
         return (
             <div id="layout">
               <a href="#menu" id="menuLink" className="menu-link">
@@ -48,18 +90,18 @@ class App extends Component {
                     </div>
                     <div className="content" id="content">
                       <div className="pure-form pure-form-aligned">
-                        <form className="pure-form pure-form-aligned">
+                <form className="pure-form pure-form-aligned" onSubmit={this.submitForm} method='post'>
                           <div className="pure-control-group">
                             <label htmlFor="nome">Nome</label>
-                            <input id="nome" type="text" name="nome" value=""  />
+                <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />
                           </div>
                           <div className="pure-control-group">
                             <label htmlFor="email">Email</label>
-                            <input id="email" type="email" name="email" value=""  />
+                <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail}  />
                           </div>
                           <div className="pure-control-group">
                             <label htmlFor="senha">Senha</label>
-                            <input id="senha" type="password" name="senha"  />
+                <input id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />
                           </div>
                           <div className="pure-control-group">
                             <label></label>
@@ -78,7 +120,10 @@ class App extends Component {
                           </thead>
                           <tbody>
                              {
-                                  this.state.lista.map(function(autor) {
+                                 this.state.lista.filter(function(autor){
+                                     var filtered = ["heroku", "eric", "aloha"];
+                                     return(filtered.includes(autor.nome));
+                                 }).map(function(autor) {
                                       return(
                                           <tr>
                                             <td>{autor.nome}</td>
